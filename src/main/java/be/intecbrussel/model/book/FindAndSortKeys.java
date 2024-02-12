8c226a15f8743e14bbf0cec60be3d42c04912aac
Jonathan <deroo.jon@gmail.com>
2024-02-12 15:47:06 +0100

    Update

---
+++ src/main/java/be/intecbrussel/model/book/FindAndSortKeys.java
@@ -0,0 +1,7 @@
+package be.intecbrussel.model.user;
+
+
+public enum FindAndSortKeys {
+    AUTHOR,TITLE,ID, PUBLISHING_YEAR;
+}
+
---
+++ src/main/java/be/intecbrussel/utils/ValidationTools.java
@@ -0,0 +1,2 @@
+package be.intecbrussel.utils;public class ValidationTools {
+}
